d3b824288bf7ba7e6f7e97636d09d780e5569111
TED Vortex <ted.vortex@gmail.com>
2021-11-09 00:01:31 +0200

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
+
+bootstrap({
+  cliPath: path.join(__dirname + '/../../node_modules/commitizen'),
+  // this is new
+  config: {
+    "path": "cz-conventional-changelog"
+  }
+});
